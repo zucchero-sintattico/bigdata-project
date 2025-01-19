92d6744401dc5a7493eb3f11d5ee1b9c7ba9d527
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2025-01-19 18:06:01 +0100

    refactor: removed refuse

--- src/main/scala/CsvParser.scala
+++ src/main/scala/CsvParser.scala
@@ -7,30 +7,6 @@ object CsvParser {
   val pipeRegex = "\\|(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"
   val quotes = "\""
 
-  /** Convert from timestamp (String) to year (Int) */
-  def yearFromTimestamp(timestamp: String): Int = {
-    val cal = Calendar.getInstance()
-    cal.setTimeInMillis(timestamp.trim.toLong * 1000L)
-    cal.get(Calendar.YEAR)
-  }
-
-  /** Function to parse movie records
-   *
-   * @param line line that has to be parsed
-   * @return tuple containing movieId, title and genres, none in case of input errors
-   */
-  def parseMovieLine(line: String): Option[(Long, String, String)] = {
-    try {
-      val input = line.split(commaRegex)
-      var title = input(1).trim
-      title = if (title.startsWith(quotes)) title.substring(1) else title
-      title = if (title.endsWith(quotes)) title.substring(0, title.length - 1) else title
-      Some(input(0).trim.toLong, title, input(2).trim)
-    } catch {
-      case _: Exception => None
-    }
-  }
-
   // (PID, playlist_name, num_followers)
   def parsePlayListLine(line: String): Option[(String, String, Int)] = {
     try {
