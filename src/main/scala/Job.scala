dc77326a040d0d17fc8004316e57c534f8f579ff
Tommaso <tommaso.brini@studio.unibo.it>
2025-01-22 12:11:24 +0100

    feat: implement jobs in scala section

--- src/main/scala/Job.scala
+++ src/main/scala/Job.scala
@@ -1,21 +1,22 @@
-import org.apache.spark.sql.functions.count
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import utils.Commons
+import utils.{Commons, Config}
+
+import scala.collection.mutable
 
 object Job {
 
 
   val path_to_datasets = "/datasets/processed/"
 
-  val path_tracks = path_to_datasets + "tracks.csv"
-  val path_playlists = path_to_datasets + "playlists.csv"
-  val path_tracks_in_playlist = path_to_datasets + "tracks_in_playlist.csv"
-  val path_artists = path_to_datasets + "artists.csv"
+  private val path_tracks = path_to_datasets + "tracks.csv"
+  private val path_playlists = path_to_datasets + "playlists.csv"
+  private val path_tracks_in_playlist = path_to_datasets + "tracks_in_playlist.csv"
+  private val path_artists = path_to_datasets + "artists.csv"
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Spotify job").getOrCreate()
     val sqlContext = spark.sqlContext // needed to save as CSV
-    import sqlContext.implicits._
+
 
     //    if (args.length < 2) {
     //      println("The first parameter should indicate the deployment mode (\"local\" or \"remote\")")
@@ -29,7 +30,7 @@ object Job {
       writeMode = "remote"
     }
     //    val job = args(1)
-    val job = "1"
+    val job = "4"
     val rddTracks = spark.sparkContext.
       textFile(Commons.getDatasetPath(deploymentMode, path_tracks)).
       flatMap(CsvParser.parseTrackLine)
@@ -85,6 +86,110 @@ object Job {
     }
     else if (job == "2") {
       // Job Tommi
+
+      // RDD of (pid, trackUri)
+      val playlistTracks = rddTracksInPlaylist.map(x => (x._1, x._2))
+
+      // auto-join on track_in_playlist to get all song pairs in the same playlist
+      val rddTrackPairs = playlistTracks.join(playlistTracks)
+        .filter { case (_, (track1, track2)) => track1 != track2 }
+        .map { case (_, (track1, track2)) => ((track1, track2), 1)}
+
+      // => rdd di forma ((track1,track2), 1) use to count the occurrences
+
+      val occurrencesCount = rddTrackPairs.reduceByKey(_ + _)
+
+      // take the pair with the highest value for each track
+      val mostOccurrencesPair = occurrencesCount
+        .map { case ((track1, track2), count) => (track1, (track2, count))}
+        .reduceByKey { case ((track2, count1), (track3, count2)) => if (count1 > count2) (track2, count1) else (track3, count2)}
+
+      // join on rddTracks for track names
+      val trackDetails = rddTracks.map(x => (x._1, x._2))
+      val firstResult = mostOccurrencesPair.join(trackDetails)
+        .map { case (track1, ((track2, count), track1Name)) => (track1, track1Name, track2, count)}
+
+      val result = firstResult
+        .map { case (track1, name, track2, count) => (track2, (track1, name, count)) }
+        .join(trackDetails)
+        .map { case (track2, ((track1, name, count), track2Name)) => (track1, name, track2, track2Name, count) }
+
+      result.saveAsTextFile(Config.projectDir + "output/result")
+
+    }
+    else if (job == "3") {
+      // Job Gigi Optimized
+    }
+    else if (job == "4") {
+      // Job Tommi optimized
+
+      val playlistToTracks = rddTracksInPlaylist
+        .map(x => (x._1, x._2))
+
+      val coTracksByPlaylist = playlistToTracks
+        .groupByKey() // group the track for same playlist
+        .flatMap { case (_, tracks) =>
+          val trackList = tracks.toList
+          for {
+            track <- trackList
+            coTrack <- trackList if track != coTrack
+          } yield (track, coTrack) // generates pair (track, coTrack)
+        }
+
+
+      val occurrenceCount = coTracksByPlaylist
+        .aggregateByKey(mutable.Map[String, Int]())(    // ogni chiave (traccia) viene inizializzata con una mappa che terrà il count delle sue co-tracce
+          (acc, coTrack) => {
+            acc(coTrack) = acc.getOrElse(coTrack, 0) + 1
+            acc
+          },   // per ogni chiave vengono iterati i valori associati (coTrack) e incrementato il count
+          (map1, map2) => {
+            map2.foreach { case (coTrack, count) =>
+              map1(coTrack) = map1.getOrElse(coTrack, 0) + count
+            }
+            map1
+          }
+        )
+        .mapValues(_.toMap)
+
+      /*
+      val occurrenceCount = coTracksByPlaylist
+        .map { case (track, coTrack) => (track, Map(coTrack -> 1)) } // Mappa tracce a un conteggio iniziale
+        .aggregateByKey(Map[String, Int]())(
+          (acc, value) => { // Combinatore locale
+            value.foldLeft(acc) { case (map, (coTrack, count)) =>
+              map + (coTrack -> (map.getOrElse(coTrack, 0) + count))
+            }
+          },
+          (map1, map2) => { // Combinatore globale
+            map2.foldLeft(map1) { case (map, (coTrack, count)) =>
+              map + (coTrack -> (map.getOrElse(coTrack, 0) + count))
+            }
+          }
+        )
+
+       */
+      val mostCooccurringTrackPerTrack = occurrenceCount
+        .mapValues { occurrences =>
+          occurrences.maxBy(_._2) // Trova la traccia con il conteggio massimo
+        }
+
+      // Mappa i dettagli delle tracce
+      val trackDetail = rddTracks.map(line => (line._1, line._2))
+
+      // Aggiungi i dettagli
+      val enrichedResults = mostCooccurringTrackPerTrack
+        .join(trackDetail) // Aggiungi il nome della traccia principale
+        .map { case (trackUri, ((coTrackUri, count), trackName)) =>
+          (coTrackUri, (trackUri, trackName, count))
+        }
+        .join(trackDetail) // Aggiungi il nome della traccia co-occurrente
+        .map { case (coTrackUri, ((trackUri, trackName, count), coTrackName)) =>
+          (trackUri, trackName, coTrackUri, coTrackName, count)
+        }
+
+      enrichedResults.saveAsTextFile(Config.projectDir + "output/result")
+
     }
   }
 
