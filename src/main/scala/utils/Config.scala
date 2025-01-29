e753cf7e4ab079c74298ee42022ee2cc8b6a1a29
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2025-01-29 16:33:11 +0100

    fix(config): add slash to project dir path

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -4,7 +4,7 @@ object Config {
 
   // The local directory containing this repository
   //val projectDir = "/Users/tbrin/Desktop/bigdata-project/"
-  val projectDir: String = "/Users/giggino/Desktop/bigdata-project.nosync"
+  val projectDir: String = "/Users/giggino/Desktop/bigdata-project.nosync/"
   // The name of the shared bucket on AWS S3 to read datasets
   val s3sharedBucketName: String = "unibo-bd2425-egallinucci" // TODO: change this
   // The name of your bucket on AWS S3
